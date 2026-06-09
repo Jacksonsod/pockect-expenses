9da5430dd7112d95a3a30fcf235f5ba9a0944a5f
Edison <hirwae15@gmail.com>
2026-06-09 13:33:11 +0200

    feat: added the header and cards

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('expenses.index');
+});
+
 Route::resource('expenses', ExpenseController::class);
